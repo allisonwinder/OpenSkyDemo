f0822842432efe8e99a79a3d1b7e81ce07983b6e
allisonwinder <allisonwinder1@gmail.com>
2024-11-05 08:37:34 -0700

    Initial Commit

---
+++ OpenSkyDemo/OpenSkyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenSkyDemoApp.swift
+//  OpenSkyDemo
+//
+//  Created by IS 543 on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenSkyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
